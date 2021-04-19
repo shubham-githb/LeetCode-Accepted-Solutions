8d79bd08650356151aadb4dfaf251bbb1bbcb55e
shubham-githb <72751187+shubham-githb@users.noreply.github.com>
2021-04-19 13:51:48 +0530

    Time: 88 ms (6.50%), Space: 10.7 MB (33.06%) - LeetHub

---
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        map<char,int> hmap;
+        for(int i=0;i<n;i++) 
+            hmap[s[i]]++;
+        for(int i=0;i<n;i++)
+        {
+            if(hmap[s[i]]==1) return i;
+        }
+        return -1;
+        
+    }
+};
