4f6565ee1e714e76ba74f3a1e48648f9b2d4f98b
shubham-githb <72751187+shubham-githb@users.noreply.github.com>
2021-04-30 13:01:30 +0530

    Time: 4 ms (55.56%), Space: 11.1 MB (21.83%) - LeetHub

---
+++ cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int Parentheight(TreeNode* root,int &parent,int n,int h ){
+        if(root==NULL)
+            return 0;
+        if(root->val==n){
+            return h;
+        }
+        parent = root->val;
+        int left =  Parentheight(root->left,parent,n,h+1);
+        if(left)
+            return left;
+        parent = root->val;
+        int right = Parentheight(root->right,parent,n,h+1);
+        return right;
+        
+    }
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
+        int xParent = -1;
+        int xHeight = Parentheight(root,xParent,x,0);
+            
+        int yParent = -1;
+        int yHeight = Parentheight(root,yParent,y,0);
+        
+        if(yParent!=xParent and xHeight==yHeight)
+            return true;
+        return false;
+        
+    }
+};
