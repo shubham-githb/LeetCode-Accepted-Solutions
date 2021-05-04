20928a883c2672176cb3810e2e31dfb6f1e79136
shubham-githb <72751187+shubham-githb@users.noreply.github.com>
2021-05-04 15:21:52 +0530

    Time: 32 ms (68.21%), Space: 32.7 MB (69.92%) - LeetHub

---
+++ delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+    int inorder_predecessor(TreeNode* root){
+        root=root->left;
+        while(root->right){
+            root=root->right;
+        }
+        return root->val;
+    }
+    
+    int inorder_successor(TreeNode* root){
+        root=root->right;
+        while(root->left){
+            root=root->left;
+        }
+        return root->val;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)
+            return NULL;
+        if(root->val>key){
+            root->left=deleteNode(root->left,key);
+        }
+        else if(root->val<key){
+            root->right = deleteNode(root->right,key);
+        }
+        else{
+            if(root->left==NULL & root->right==NULL) root=nullptr;
+            else if(root->left){
+                root->val = inorder_predecessor(root);
+                root->left = deleteNode(root->left,root->val);
+            }
+            else{
+                root->val = inorder_successor(root);
+                root->right= deleteNode(root->right,root->val);
+            }
+        }
+        
+        return root;
+    }
+};
