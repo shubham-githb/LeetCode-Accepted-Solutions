f80a1dc389c48cc89213b26c34220a8d940aa3ed
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2021-07-03 13:16:53 +0530

    Time: 4 ms (93.02%), Space: 13 MB (79.50%) - LeetHub

---
+++ rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        queue<pair<int,int>> rotten;
+        int fresh =0;
+        int t=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==2) rotten.push({i,j});
+                else if(grid[i][j]==1) fresh++;
+            }
+        }
+        
+         while(!rotten.empty()){
+            int num = rotten.size();
+            for(int i = 0; i < num; ++i){
+                int x = rotten.front().first, y = rotten.front().second;
+                rotten.pop();
+                if(x > 0 && grid[x-1][y] == 1){ grid[x-1][y] = 2; fresh--; rotten.push({x-1, y});};
+                if(y > 0 && grid[x][y-1] == 1){ grid[x][y-1] = 2; fresh--; rotten.push({x, y-1});};
+                if(x < r-1 && grid[x+1][y] == 1){ grid[x+1][y] = 2; fresh--; rotten.push({x+1, y});};
+                if(y < c-1 && grid[x][y+1] == 1){ grid[x][y+1] = 2; fresh--; rotten.push({x, y+1});};
+            }
+            if(!rotten.empty()) t++;
+        }
+        return (fresh == 0) ? t : -1;
+    }
+};
