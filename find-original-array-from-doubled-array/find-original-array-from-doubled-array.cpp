f1afa388a74241891d762e879673b305c51ffb8b
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2021-09-19 12:37:42 +0530

    Time: 388 ms (43.75%), Space: 142.4 MB (37.50%) - LeetHub

---
+++ find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& nums) {
+        unordered_map<int,int> hmap;
+        for(auto x: nums){
+            hmap[x]++;
+        }
+        sort(nums.begin(), nums.end());
+
+        vector<int> res;
+        for(auto ele: nums){
+            if(hmap.find(ele*2)!=hmap.end() and hmap[ele]!=0 and hmap[ele*2]!=0){
+                hmap[ele]--;
+                hmap[ele*2]--;
+                res.push_back(ele);
+            }
+        }
+          
+        for(auto i : hmap){
+            if(i.second != 0){
+                return vector<int>();
+            }
+        } 
+        
+        return res;
+    }
+};
