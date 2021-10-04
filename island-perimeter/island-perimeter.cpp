edea893a4ef3975fe50fd836b0d9e5d1efdf8a40
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2021-10-04 11:02:45 +0530

    Time: 96 ms (65.80%), Space: 96.6 MB (28.56%) - LeetHub

--- island-perimeter/island-perimeter.cpp
+++ island-perimeter/island-perimeter.cpp
@@ -1,25 +1,32 @@
 class Solution {
+    void dfs(vector<vector<int>>& grid,int i, int j , int &p){
+        grid[i][j] = 2;
+        if(i==0 || grid[i-1][j]==0) p++;
+        if(i==grid.size()-1 || grid[i+1][j]==0) p++;
+        if(j==0 || grid[i][j-1]==0) p++;
+        if(j==grid[0].size()-1|| grid[i][j+1]==0) p++;
+        
+        if(i>0 and grid[i-1][j]==1) dfs(grid,i-1,j,p);
+        if(i<grid.size()-1 and grid[i+1][j]==1) dfs(grid,i+1,j,p);
+        if(j>0 and grid[i][j-1]==1) dfs(grid,i,j-1,p);
+        if(j<grid[0].size()-1 and grid[i][j+1]==1) dfs(grid,i,j+1,p);
+        
+        
+    }
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int p=0;
-        int r= grid.size();
+        int p = 0;
+        int r = grid.size();
         int c = grid[0].size();
-        
-        
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j]==0)
-                    continue;
-                p = p+4;
-                if(i>0) p -= grid[i-1][j];
-                if(j>0) p -= grid[i][j-1];
-                if(i<r-1) p -= grid[i+1][j];
-                if(j<c-1) p -= grid[i][j+1];
+                if(grid[i][j]==1){
+                    dfs(grid,i,j,p);
+                }
             }
         }
         
         return p;
-        
         
     }
 };
