ff41572a28a2a02be210129da5d16404e827f91f
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2021-11-17 11:00:15 +0530

    Time: 20 ms (35.82%), Space: 13.7 MB (41.54%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,44 +1,26 @@
 class Solution {
-    struct node {
-        int no;
-        int freq;
-        node(int a , int b)
-        {
-            no = a;
-            freq=b;
-        }
-        
-    };
-    
-    struct compare {
-        bool operator() (node const& a, node const& b){
-            return a.freq<b.freq;
-        }
-    };
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> h;
-        vector<int> ans;
-       
-        
-        for(int i : nums ){
-            h[i]+= 1;
+        priority_queue<pair<int,int>> pq;
+        vector<int> res;
+        unordered_map<int,int> hmap;
+        for(auto x: nums){
+            hmap[x]++;
         }
         
-        priority_queue<node,vector<node>,compare> pq;
-        
-        for(auto x: h){
-            pq.push(node(x.first,x.second));
+        for(auto i: hmap){
+            pq.push(make_pair(i.second,i.first));
         }
-        
         
         while(k--){
-            node temp = pq.top();
+            res.push_back(pq.top().second);
             pq.pop();
-            ans.push_back(temp.no);
         }
         
-        return ans;
+        
+        return res;
+        
+        
         
     }
 };
