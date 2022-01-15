cc529dc0d4e6c3df4b1975c3465ae1bb8e056979
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2022-01-16 00:38:36 +0530

    Time: 206 ms (9.45%), Space: 10.4 MB (88.92%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-       long long subArrayRanges(vector<int>& A) {
-        long long res = 0;
-        for (int i = 0; i < A.size(); i++) {
-            int ma = A[i], mi = A[i];
-            for (int j = i; j < A.size(); j++) {
-                ma = max(ma, A[j]);
-                mi = min(mi, A[j]);
-                res += ma - mi;
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        long long  res = 0;
+        for(int i=0;i<n;i++){
+            int maxi = nums[i];
+            int mini = nums[i];
+            for(int j=i;j<n;j++){
+                maxi = max(maxi,nums[j]);
+                mini = min(mini,nums[j]);
+                res += maxi-mini;
             }
         }
         return res;
     }
 };
+
+    // long long subArrayRanges(vector<int>& A) {
+    //     long long res = 0;
+    //     for (int i = 0; i < A.size(); i++) {
+    //         int ma = A[i], mi = A[i];
+    //         for (int j = i; j < A.size(); j++) {
+    //             ma = max(ma, A[j]);
+    //             mi = min(mi, A[j]);
+    //             res += ma - mi;
+    //         }
+    //     }
+    //     return res;
+    // }
