3b754177d31390e6ebb065b578cf033a67cefd13
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2022-02-15 11:08:49 +0530

    Time: 668 ms (8.84%), Space: 10.3 MB (70.88%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+   int countPrimes(int n) {
+        if(n<2) return 0;
+        int ans=0;
+        vector<bool> pp(n,true);
+        pp[0]=pp[1]=false;
+        for(int i=2;i*i<=n;++i) {
+            if(pp[i]==true) {
+                for(int j=i+i;j<n;j+=i) {
+                    pp[j]=false;
+                }
+            }
+        }
+        return count(pp.begin(),pp.end(),true);
+}
+};
