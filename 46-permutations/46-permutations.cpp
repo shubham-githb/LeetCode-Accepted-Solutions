a9395f7843d0327837c167e8bef7d16ffbb00ce5
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2022-06-27 13:28:36 +0530

    Time: 5 ms (49.36%), Space: 7.7 MB (55.59%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,19 +1,36 @@
 class Solution {
-    void solve(vector<int> &ds,vector<int>& nums,  vector<vector<int>>& ans,vector<int>&freq){
+//     void solve(vector<int> &ds,vector<int>& nums,  vector<vector<int>>& ans,vector<int>&freq){
+//         if(ds.size()==nums.size()){
+//             ans.push_back(ds);
+//             return;
+//         }
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(freq[i]==0){
+//                 ds.push_back(nums[i]);
+//                 freq[i]=1;
+//                 solve(ds,nums,ans,freq);
+//                 freq[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+//     }
+    void solve(vector<int> &ds, vector<int> &nums, vector<vector<int>>& ans, vector<int> & freq){
         if(ds.size()==nums.size()){
             ans.push_back(ds);
             return;
         }
-        
-        for(int i=0;i<nums.size();i++){
-            if(freq[i]==0){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                solve(ds,nums,ans,freq);
-                freq[i]=0;
-                ds.pop_back();
+            int n = nums.size();
+            for(int i=0;i<n;i++){
+                if(freq[i]==0){
+                    ds.push_back(nums[i]);
+                    freq[i]=1;
+                    solve(ds,nums,ans,freq);
+                    freq[i]=0;
+                    ds.pop_back();
+                    
+                }
             }
-        }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
