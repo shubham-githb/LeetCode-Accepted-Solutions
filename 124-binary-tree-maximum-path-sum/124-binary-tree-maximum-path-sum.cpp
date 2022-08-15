ab25e66e916b2da8c34dabf2c674d8db01d1a5e0
Shubham Kumar <72751187+shubham-githb@users.noreply.github.com>
2022-08-15 21:34:38 +0530

    Time: 22 ms (97.52%), Space: 28.2 MB (23.20%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -11,9 +11,32 @@
  */
 class Solution {
     int maxPath(TreeNode* root, int &result){
+//         if(root==NULL){
+//             return 0;
+//         }
+//         int left = maxPath(root->left,result);
+//         int right = maxPath(root->right,result);
+        
+//         int max1 = max(max(left,right)+root->val,root->val);
+//         int max2 = max(max1,(left+right+root->val));
+//         result = max(max2,result);
+        
+// //         return max1;
+//         if(root==NULL){
+//             return 0;
+//         }
+//         int left = maxPath(root->left,result);
+//         int right = maxPath(root->right,result);
+        
+//         int max1 = max(max(left,right)+root->val,root->val);
+//         int max2 = max(max1,(left+right+root->val));
+//         result = max(max2,result);
+        
+//         return max1;
         if(root==NULL){
             return 0;
         }
+        
         int left = maxPath(root->left,result);
         int right = maxPath(root->right,result);
         
@@ -26,10 +49,17 @@ class Solution {
     }
 public:
     int maxPathSum(TreeNode* root) {
+//         if(root==NULL){
+//             return 0;
+//         }
+//         int res=INT_MIN;
+//         maxPath(root,res);
+        
+//         return res;
         if(root==NULL){
             return 0;
         }
-        int res=INT_MIN;
+        int res = INT_MIN;
         maxPath(root,res);
         
         return res;
